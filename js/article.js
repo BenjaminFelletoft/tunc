86e8c65f8af5e44d59bc213d471458a9d32b6e3a
Asger Steenfeldt Nielsen <ASni.skp>
2019-01-08 10:28:33 +0100

    article module 2

---
+++ js/article.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $.ajax({
+        url:'../resources/database.php',
+        data:{action:'GetArticle', id:article_id},
+        method:'POST',
+        dataType:'json',
+        success:PrintArticles
+    });
+});
+
+function PrintArticles(article){
+    article = JSON.parse(article);
+    $('.article-title').html(article['titel']);
+    $('.article-content').append(article['article']);
+    $('.article-date').html(article['created_at']);
+    $('.article-author').html(article['author']);
+}
---
+++ resources/database.php
@@ -0,0 +1,86 @@
+<?php
+
+	$action = isset($_POST['action']) ? $_POST['action'] : "";
+	switch($action){
+		case 'GetArticle':
+			echo json_encode(Database::select("SELECT * FROM articles WHERE id = ?", array($_POST['id'])));
+		break;
+	}
+
+	class Database {
+
+		private static $db;
+		private $connection;
+
+		private function __construct() {
+			try 
+			{
+				//Database informationer skal ændres
+				$this->connection = new PDO('mysql:host=localhost;dbname=connyinstrukdb;', 'root', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8") );
+				//--------------------------------------------------------------------------------------------------------------
+				$this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				$this->connection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false );
+			}
+			catch (PDOException $e)
+			{
+				dnd($e);
+			}
+		
+		}
+
+		function __destruct() {
+			$this->connection = null;
+		}
+
+		public static function getConnection() {
+			if (self::$db == null) {
+				self::$db = new self();
+			}
+			return self::$db->connection;
+		}
+		public static function select($sql, $params) {
+			$stmt = self::getConnection()->prepare($sql);
+			foreach ($params as $key => $value) {
+				if (is_null($value))
+					$value = '';
+				if (gettype($value) == "string") 
+					$stmt->bindParam($key + 1, $params[$key], PDO::PARAM_STR);
+				else 
+					$stmt->bindParam($key + 1, $params[$key], PDO::PARAM_INT);
+			}
+			$stmt->execute();
+			return $stmt->fetchAll(PDO::FETCH_ASSOC);
+		}
+		public static function insert($sql, $params) {
+			$stmt = self::getConnection()->prepare($sql);
+			foreach ($params as $key => $value) {
+				if (empty($value))
+					$value = '';
+				if (gettype($value) == "string")
+					$stmt->bindParam($key + 1, $params[$key], PDO::PARAM_STR);
+				else
+					$stmt->bindParam($key + 1, $params[$key], PDO::PARAM_INT);
+			}
+			$stmt->execute();
+			return self::getConnection()->lastInsertId();
+		}
+		public static function update($sql, $params) {
+			$stmt = self::getConnection()->prepare($sql);
+			foreach ($params as $key => $value) {
+				if (empty($value))
+					$value = '';
+				if (gettype($value) == "string")
+					$stmt->bindParam($key + 1, $params[$key], PDO::PARAM_STR);
+				else
+					$stmt->bindParam($key + 1, $params[$key], PDO::PARAM_INT);
+			}
+			$stmt->execute();
+		}
+		public static function selectRow($sql, $params) {
+			$result = self::select($sql, $params);
+			if (isset($result[0]))
+				return $result[0];
+			else 
+				return false;
+		}
+	}
