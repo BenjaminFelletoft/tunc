06610ff59c253cc7713ab9f0c33750492d6f245c
Kasper Riis Pedersen <kaspermail1999@gmail.com>
2019-01-08 13:55:38 +0100

    new ip address

--- resources/database.php
+++ resources/database.php
@@ -21,7 +21,7 @@
 			try 
 			{
 				//Database informationer skal ændres
-				$this->connection = new PDO('mysql:host=192.168.159.35;dbname=tunc;', 'root', 'Password1', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8") );
+				$this->connection = new PDO('mysql:host=192.168.159.39;dbname=tunc;', 'root', 'Password1', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8") );
 				//--------------------------------------------------------------------------------------------------------------
 				$this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 				$this->connection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false );
