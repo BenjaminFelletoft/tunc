102a2d21a397c23f909b6b8d313cae842abf2292
asnielsen789 <40421965+asnielsen789@users.noreply.github.com>
2019-01-08 10:03:38 +0100

    Add files via upload

---
+++ article.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/article.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="js/article.js"></script>
+</head>
+<body>
+    <div class="article-container col-lg-8 col-lg-offset-2">
+        <div class="article-header">
+            <h1 class="article-title">Title</h1>
+            <div class="panel panel-default article-subtitle">
+                <p class="author" style="display: inline">author</p>
+                <p class="date" style="display: inline">date</p>
+            </div>
+        </div>
+        <div class="article-content">
+            Content
+        </div>
+    </div>
+</body>
+</html>
