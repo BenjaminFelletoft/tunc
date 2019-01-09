180cef4b0e52c3beee5061f3083be7fe22a3224d
Leon Svane Jensen <LSJe.skp>
2019-01-09 11:39:15 +0100

    search frontpage

--- js/frontpage.js
+++ js/frontpage.js
@@ -10,6 +10,25 @@ $(document).ready(function(){
         },
     });
 });
+function searchArticles() {
+    // Declare variables
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('searchInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("articleList");
+    li = ul.getElementsByTagName('li');
+  
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("h3")[0];
+      txtValue = a.textContent || a.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+      } else {
+        li[i].style.display = "none";
+      }
+    }
+  }
 
 function PrintArticleLinks(articles){
     var articleCount = articles.length;
