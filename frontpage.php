5a563c76a0ab47eb97af2be0c1c220b379cc9701
Kasper Riis Pedersen <kaspermail1999@gmail.com>
2019-01-09 11:06:12 +0100

    Styling changes

--- frontpage.php
+++ frontpage.php
@@ -2,11 +2,12 @@
 <?php require('layout.php') ?>
 <html>
 <body>
-    <div class="module-container col-lg-8 col-lg-offset-2">
+    <div class="module-container col-lg-8 col-lg-offset-2 jumbotron">
         <div class="module-header">
             <h1 class="module-title">Articles</h1>
         </div>
-        <div class="module-content">
+        <div class="module-content container">
+            <div class="row"></div>
         </div>
     </div>
 </body>
