6f3de5eaafba08db9fead97d069d0d0efbd125d7
Bofe.skp <BoFe.skp@plato.local>
2019-01-09 08:04:15 +0100

    style

--- create_article.php
+++ create_article.php
@@ -5,14 +5,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Create Article</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="css/create_article.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="css/style.css" />
 </head>
 <?php require('layout.php') ?>
 <body>
-    <p>Title</p><input id="title" type="text">
-    <p>Name</p><input id="name" type="text">
-    <p>Content</p><textarea id="content" name="content"></textarea>
-    <button id="save">Save</button>
+    <div class="module-container col-lg-8 col-lg-offset-2">
+        <div class="module-header">
+            <p>Title</p><input id="title" type="text">
+            <p>Name</p><input id="name" type="text">
+            <p>Content</p><textarea id="content" name="content"></textarea>
+            <button id="save">Save</button>
+        </div>
+    </div>
 </body>
 <script src="js/create_article.js"></script>
 </html>
