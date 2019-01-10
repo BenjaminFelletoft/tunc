220cf40a8e37a4f1496ee51b9f61386afaa8f1d0
Leon Svane Jensen <LSJe.skp>
2019-01-10 10:09:29 +0100

    jokes on you

--- frontpage.php
+++ frontpage.php
@@ -6,7 +6,7 @@
         <div class="module-header">
             <h1 class="module-title">Articles</h1>
             <input class="search" id="searchInput" onkeyup="searchArticles()" type="text" placeholder="Search..">
-            <h5>Sort Articles A-Z By:</h5>
+            <h5 id="sortText">Sort Articles A-Z By:</h5>
             <input class="sortButtons" type="button" onclick="sortArticlesByTitle()" value="Title">
             <input class="sortButtons" type="button" onclick="sortArticlesByAuthor()" value="Author">
             <input class="sortButtons" type="button" onclick="sortArticlesByPosted()" value="Posted On">
