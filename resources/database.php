b7978063a400acaab70c6a97288bc7489c7bfb85
jesper-jakobsenpr@hotmail.com <JeJa.skp>
2019-01-10 08:54:24 +0100

    ICON :O

--- resources/database.php
+++ resources/database.php
@@ -46,8 +46,8 @@
 			Database::update("UPDATE polloptions SET votes=votes+1 WHERE id = ?", array($_POST['id']));
 		break;
 		case 'GetComments':
-			isset($_POST['article_id']) ? $article_id = $_POST['article_id'] : $article_id = null;
-			isset($_POST['poll_id']) ? $poll_id = $_POST['poll_id']: $poll_id = null;
+			isset($_POST['article_id']) ? $article_id = $_POST['article_id'] : $article_id;
+			isset($_POST['poll_id']) ? $poll_id = $_POST['poll_id']: $poll_id;
 			$data = null;
 			if(isset($article_id)){
 				$data = Database::select("SELECT * FROM comments WHERE article_id = ?", array($article_id));
@@ -57,8 +57,8 @@
 			echo json_encode($data);
 		break;
 		case 'CreateComment':
-			isset($_POST['article_id']) ? $article_id = $_POST['article_id'] : $article_id = null;
-			isset($_POST['poll_id']) ? $poll_id = $_POST['poll_id']: $poll_id = null;
+			isset($_POST['article_id']) ? $article_id = $_POST['article_id'] : $article_id;
+			isset($_POST['poll_id']) ? $poll_id = $_POST['poll_id']: $poll_id;
 			$comment = $_POST['comment'];
 			$author = $_POST['author'];
 			$data = null;
