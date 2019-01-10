fcdf097408ed8d28fbfe98c54ed5ded123db4da0
Asger Steenfeldt Nielsen <ASni.skp>
2019-01-10 10:29:02 +0100

    sprint 5

--- comments.php
+++ comments.php
@@ -1,9 +1,5 @@
 <!DOCTYPE html>
-<head>
-<link rel="stylesheet" type="text/css" media="screen" href="css/style.css" />
-</head>
 <html>
-    <?php require('layout.php'); ?>
     <body>
         <div class="module-container col-lg-8 col-lg-offset-2 jumbotron">
             <div class="comments-content">
@@ -12,8 +8,8 @@
                     <label for="author" class="h4" style="float:left">Author</label>
                     <input type="text" id="author" class="form-control" style="max-width:30%">
                     <label for="author" class="h4" style="float:left">Your Comment</label>
-                    <textarea class="form-control" id="exampleFormControlTextarea3" rows="7"></textarea>
-                    <button type="button" class="btn btn-success" style="float:left">Comment</button>
+                    <textarea class="form-control" id="comment-content" rows="7"></textarea>
+                    <button id="save-comment" type="button" class="btn btn-success" style="float:left">Comment</button>
                 </div>
             </div>
         </div>
