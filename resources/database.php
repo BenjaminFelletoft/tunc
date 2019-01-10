aeefcae13023bd70c542f27e969c6017b549ddb8
Bofe.skp <BoFe.skp@plato.local>
2019-01-10 12:04:06 +0100

    fix

--- resources/database.php
+++ resources/database.php
@@ -80,18 +80,14 @@
 			echo json_encode($data);
 		break;
 		case 'DeleteArticle':
-			Database::update("DELETE FROM comments WHERE article_id = ?", array($_POST['id']));
+			//Database::update("DELETE FROM comments WHERE article_id = ?", array($_POST['id']));
 			$data = Database::update("DELETE FROM articles WHERE id = ?", array($_POST['id']));
 			echo json_encode($data);
 		break;
 		case 'DeletePoll':
 			Database::update("DELETE FROM polloptions WHERE poll_id = ?", array($_POST['id']));
-<<<<<<< HEAD
 			Database::update("DELETE FROM comments WHERE poll_id = ?", array($_POST['id']));
 			$data = Database::update("DELETE FROM poll WHERE id = ?", array($_POST['id']));
-=======
-				$data = Database::update("DELETE FROM poll WHERE id = ?", array($_POST['id']));
->>>>>>> 9cdabec0c99fe308f0f1c90a265056d3aa96f1bf
 			echo json_encode($data);
 		break;
 	}
