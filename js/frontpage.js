02683169596d26e38d4083c611a735617ffc18e2
Kasper Riis Pedersen <kaspermail1999@gmail.com>
2019-01-10 10:34:46 +0100

    added polls to front page & fixed posted date font size

--- js/frontpage.js
+++ js/frontpage.js
@@ -1,13 +1,34 @@
-$(document).ready(function(){
-    $.ajax({
+$(document).ready(function() {
+    var posts = [];
+
+    var articleCall = $.ajax({
         url:'resources/database.php',
         data:{action:'GetAllArticles'},
         type:'POST',
         dataType:'json',
-        success:PrintArticleLinks,
+        success: function (data) {
+            generateArticleLinks(data, posts);
+        },
         error: function (data) {
             console.log(data);
         },
+    });
+
+    var pollCall = $.ajax({
+        url:'resources/database.php',
+        data:{action:'GetAllPolls'},
+        type:'POST',
+        dataType:'json',
+        success: function (data) {
+            generatePollLinks(data, posts);
+        },
+        error: function (data) {
+            console.log(data);
+        },
+    });
+
+    $.when(articleCall, pollCall).done(function() {
+        printPosts(posts);
     });
 });
 function searchArticles() {
@@ -27,29 +48,64 @@ function searchArticles() {
     }
 }
 
-function PrintArticleLinks(articles){
-    var articleCount = articles.length;
+function generateArticleLinks(articles, postList){
     articles.forEach(article => {
         var articleContainer = $("<li></li>").append(
             $("<div></div>").addClass("col-lg-8 article").attr("id", article["id"])
+            .on("click", function() {
+                window.location.href = "article.php?article_id="+article["id"];
+            })
             .append($("<h3></h3>").text(article["title"]))
+            .append($("<p></p>").addClass("post-label label label-default").text("ARTICLE"))
             .append($("<p></p>").addClass("lead").text("by " + article["author"]))
             .append($("<hr>"))
-            .append($("<p></p>").text("Posted on " + article["created_at"]))
+            .append($("<p></p>").addClass("posted-date").text("Posted on " + article["created_at"]))
             .append($("<hr>"))
-            .append($("<div></div>").addClass("link-content").html(article["article"])));
+            .append($("<div></div>").addClass("link-content").html(article["article"]))
+        ).data("date", article["created_at"]);
         
-        $(".module-content").append(articleContainer);
-
-        if (articles.indexOf(article) + 1 !== articleCount) {
-            $("module-content").append($("<hr>"));
-        }
-
-        $('#'+article["id"]).click(function(){
-            window.location.href = "article.php?article_id="+article["id"];
-        });
+        postList.push(articleContainer);
     });
 }
+
+function generatePollLinks(polls, postList) {
+    polls.forEach(poll => {
+        var pollContainer = $("<li></li>").append(
+            $("<div></div>").addClass("col-lg-8 article").attr("id", poll["id"])
+            .on("click", function() {
+                window.location.href = "poll.php?poll_id="+poll["id"];
+            })
+            .append($("<h3></h3>").text(poll["title"]))
+            .append($("<p></p>").addClass("post-label label label-primary").text("POLL"))
+            .append($("<p></p>").addClass("lead").text("by " + poll["author"]))
+            .append($("<hr>"))
+            .append($("<p></p>").addClass("posted-date").text("Posted on " + poll["date"]))
+            .append($("<hr>"))
+            .append($("<div></div>").addClass("link-content").html(poll["description"]))
+        ).data("date", poll["date"]);
+        
+        postList.push(pollContainer);
+    });
+}
+
+function printPosts(posts) {
+    posts.sort(function(a, b) {
+        var firstDate = new Date(a.data("date"));
+        var lastDate = new Date(b.data("date"));
+
+        if (firstDate > lastDate) {
+            return -1;
+        }
+        else if (firstDate < lastDate) {
+            return 1;
+        }
+
+        return 0;
+    });
+
+    $(".module-content").append(posts);
+}
+
 function sortArticlesByTitle(){
     var list, c1, c2, article1, article2, i, switching, b, shouldSwitch, dir, switchcount = 0;
   list = document.getElementById("articleList");
