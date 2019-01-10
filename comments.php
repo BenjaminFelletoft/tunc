d8578d657655a970002d51ce5c31607fa46b5650
Mathias Lohmann Mikkelsen <Mathiaslohmann2004@hotmail.dk>
2019-01-10 09:58:27 +0100

    comment design

--- comments.php
+++ comments.php
@@ -1,11 +1,20 @@
 <!DOCTYPE html>
+<head>
+<link rel="stylesheet" type="text/css" media="screen" href="css/style.css" />
+</head>
 <html>
+    <?php require('layout.php'); ?>
     <body>
         <div class="module-container col-lg-8 col-lg-offset-2 jumbotron">
-            <div class="module-header">
-                <h1 class="module-title">Comments</h1>
-            </div>
             <div class="module-content">
+                <h1 class="module-title">Comments</h1>
+                <div class="link-content">
+                    <label for="author" class="h4" style="float:left">Author</label>
+                    <input type="text" id="author" class="form-control" style="max-width:30%">
+                    <label for="author" class="h4" style="float:left">Your Comment</label>
+                    <textarea class="form-control" id="exampleFormControlTextarea3" rows="7"></textarea>
+                    <button type="button" class="btn btn-success" style="float:left">Comment</button>
+                </div>
             </div>
         </div>
     </body>
