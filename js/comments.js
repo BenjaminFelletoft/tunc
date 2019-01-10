cdfa15c23215cedbc9aa2d14aabc728a566f9202
Asger Steenfeldt Nielsen <ASni.skp>
2019-01-10 11:08:05 +0100

    sprint 5

--- js/comments.js
+++ js/comments.js
@@ -28,27 +28,26 @@ function PrintComments(comments){
     });
     $('.comments-content').append(
         `<div class="comment">
-            <label for="author" class="h4" style="float:left">Author</label>
+            <label for="author" class="h4">Author</label>
             <input type="text" id="author" class="form-control" style="max-width:30%">
             <label for="comment-content" class="h4" style="float:left">Your Comment</label>
             <textarea class="form-control" id="comment-content" rows="7"></textarea>
             <button id="save-comment" type="button" class="btn btn-success" style="float:left">Comment</button>
         </div>`
     );
-}
-
-$('#save-comment').click(function(){
-    console.log("test");
-    $.ajax({
-        data: {action:'CreateComment', author:$('#author').val(), comment:$('#comment-content').val(), article_id:article_id, poll_id:poll_id},
-        type: "POST",
-        url: "resources/database.php",
-        success: function(data){
-            console.log(data);
-            location.reload();
-        },
-        error:function(data){
-            console.log(data);
-        }
+    $('#save-comment').click(function(){
+        console.log("test");
+        $.ajax({
+            data: {action:'CreateComment', author:$('#author').val(), comment:$('#comment-content').val(), article_id:article_id, poll_id:poll_id},
+            type: "POST",
+            url: "resources/database.php",
+            success: function(data){
+                console.log(data);
+                location.reload();
+            },
+            error:function(data){
+                console.log(data);
+            }
+        });
     });
-});
+}
